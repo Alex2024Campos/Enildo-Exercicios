a5040afe73ff4c79595ebbf2f1f0dff28df010d2
Alex2024Campos <alex.oliveira221@etec.sp.gov.br>
2025-10-21 17:10:42 -0300

    Correção dos exercícios 9-11 do MYSQL

---
+++ Exercicio 11.sql
@@ -0,0 +1,132 @@
+
+-- Exercício 11:
+
+create table tbCliente(
+Id int primary key auto_increment,
+NomeCli char(200) not null,
+NumEnd int not null,
+CompEnd char(50),
+CepCli int,
+foreign key (CepCli) references tbEndereco(CEP) -- Foreign Key
+);
+
+create table tbClientePF(
+CPF bigint primary key,
+RG int not null,
+RG_Dig char(1) not null,
+Nasc date not null,
+
+id int,
+foreign key (id) references tbCliente(Id)
+);
+
+create table tbClientePJ(
+CNPJ bigint primary key,
+IE bigint unique,
+
+id int,
+foreign key (id) references tbCliente(Id)
+);
+
+create table tbEndereco(
+CEP int primary key,
+Logradouro char(200) not null,
+
+BairroId int not null,
+foreign key (BairroId) references tbBairro(BairroId)
+
+CidadeId int not null,
+foreign key (CidadeId) references tbCidade(CidadeId)
+
+UFId int not null,
+foreign key (UFId) references tbEstado(UFId)
+);
+
+create tbBairro(
+BairroId int primary key auto_increment,
+Bairro char(200) not null
+);
+
+create tbCidade(
+CidadeId int primary key auto_increment,
+Cidade char(200)
+);
+
+create tbEstado(
+UFId int primary key auto_increment,
+UF char(2) not null
+);
+
+
+create table tbFornecedor(
+Codigo int primary key auto_increment,
+CNPJ bigint unique,
+Nome char(200) not null,
+Telefone bigint
+);
+
+create table tbProduto(
+CodigoBarras Numeric(14) primary key,
+Nome char(200) not null,
+Valor decimal(10,2) not null,
+QtdTotal int not null,
+Codigo int, -- Foreign Key
+);
+
+create table tbCompra(
+NotaFiscal int primary key,
+DataCompra date not null,
+ValorTotal decimal (10,2) not null,
+Qtd int not null
+
+Codigo int,
+foreign key (Codigo) references tbFornecedor(Codigo)
+);
+
+create table tbItemCompra(
+CodigoBarras Numeric(14),
+NotaFiscal int,
+
+primary key (NotaFiscal, CodigoBarras),
+
+foreign key (NotaFiscal) references tbCompra(NotaFiscal),
+
+foreign key (CodigoBarras) references tbProduto(CodigoBarras),
+
+ValorItem decimal (10,2) not null,
+Qtd int not null
+);
+
+create table tbVenda(
+NumeroVenda int primary key,
+DataVenda date not null,
+TotalVenda decimal (10,2) not null,
+
+Id bigint not null, -- Foreign Key,
+foreign key (Id) references tbCliente(Id), -- Confirmar
+ 
+NF int -- Foreign Key
+foreign key (NF) references tbNotaFiscal(NF), -- Confirmar
+); 
+
+create table tbItemVenda(
+
+NumeroVenda int,
+CodigoBarras Numeric(14),
+primary key (NumeroVenda, Numeric),
+
+foreign key (NumeroVenda) references tbVenda(NumeroVenda),
+
+
+foreign key (CodigoBarras) references tbVenda(CodigoBarras),
+
+
+ValorItem decimal(10,2) not null,
+Qtd int not null
+);
+
+create table tbNotaFiscal(
+NF primary key,
+TotalNota dedcimal(10,2),
+DataEmissao date not null
+);
---
+++ Exercicio 9.sql
@@ -0,0 +1,102 @@
+
+-- Exercício 9:
+
+create database dbBANCO; -- Exercício 9
+
+use dbBANCO;
+
+create table tbagencia(
+NumeroAgencia int primary key,
+CodBanco int,
+Endereco varchar(50) not null
+);
+
+
+create table tbconta(
+NumeroConta int primary key,
+Saldo decimal(7,2),
+TipoConta smallint,
+NumAgencia int,
+foreign key (NumAgencia) references tbagencia(NumeroAgencia)
+);
+
+create table tbcliente(
+Cpf bigint primary key,
+Nome varchar(50) not null,
+Sexo char(1) not null,
+Endereco varchar(50) not null
+);
+
+create table tbhistorico(
+DataInicio date,
+Cpf bigint,
+NumeroConta int,
+foreign key (Cpf) references tbcliente(Cpf),
+foreign key (NumeroConta) references tbconta(NumeroConta)
+);
+
+
+create table tbtelefone_cliente(
+Telefone int primary key,
+Cpf bigint
+foreign key (Cpf) references tbcliente(Cpf)
+);
+
+
+create table tbbanco(
+Codigo int primary key,
+Nome varchar(50) not null
+NumeroAgencia int,
+foreign key (NumeroAgencia) referencess tbagencia(NumeroAgencia)
+);
+insert into tbbanco 
+values
+(1, "Banco do Brasil"),
+(104, "Caixa Economica Federal"),
+(801, "Banco Escola");
+
+insert into tbagencia 
+values
+(123, 1, "Av Paulist, 78"),
+(159, 104, "Rua Liberdade, 124"),
+(401, 801, "Rua Vinte e três, 23"),
+(485, 801, "Av Marechal, 68");
+
+insert into tbcliente 
+values
+(12345678910, "Enildo", "M", "Rua Grande, 75"),
+(12345678911, "Astrogildo", "M", "Rua Pequena, 789"),
+(12345678912, "Monica", "F", "Av Larga, 148"),
+(12345678913, "Cascão", "M", "Av Principal, 369");
+
+insert into tbconta 
+values
+(9876, 456.05, 1, 123),
+(9877, 312.00, 1, 123),
+(9878, 100.00, 2, 485),
+(9879, 5589.48, 1, 401);
+
+insert into tbtelefone_cliente 
+values
+(912345678, 12345678910),
+(912345679, 12345678911),
+(912345680, 12345678912),
+(912345681, 12345678913);
+
+insert into tbhistorico
+values
+('2001-04-15', 12345678910, 9876),
+('2011-03-10', 12345678911, 9877),
+('2021-03-11', 12345678912, 9878),
+('2000-07-05', 12345678913, 9879);
+
+alter table tbcliente add email varchar(100);
+
+select *
+from tbcliente where Nome = "Monica";
+
+select *
+from tbagencia where CodBanco = "801";
+
+select *
+from tbcliente where Sexo = "M";
